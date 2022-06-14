ea667186db936be02d993b7348650172b489fd67
Harsh Koli <harshkoli201@gmail.com>
2022-06-14 13:57:31 +0530

    Added List with ItemCount

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+int itemcount = 20;
+
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
@@ -10,6 +12,15 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemCount: itemcount,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text('Item ${index + 1}'),
+          leading: const Icon(Icons.person),
+          trailing: const Icon(Icons.select_all),
+        );
+      },
+    );
   }
 }
